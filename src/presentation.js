d277131a75687e9dfa0e1d1edb29afc6218102c2
Trygve <tnordberg@smm.org>
2018-02-07 16:43:23 -0500

    Base structure for Spectacle based slide show.

---
+++ src/presentation.js
@@ -0,0 +1,98 @@
+// Import React
+import React from 'react';
+
+// Import Spectacle Core tags
+import {
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  ListItem,
+  List,
+  Quote,
+  Image,
+  Slide,
+  Text,
+} from 'spectacle';
+
+// Import theme
+import createTheme from 'spectacle/lib/themes/default';
+
+// Require CSS
+require('normalize.css');
+
+const theme = createTheme(
+  {
+    primary: '#F6C27C',
+    secondary: '#24DDB4',
+    tertiary: '#FAF97B',
+    quartenary: '#F49CB4',
+  },
+  {
+    primary: 'Georgia',
+    secondary: 'Helvetica',
+  }
+);
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Deck
+        transition={['zoom', 'slide']}
+        transitionDuration={500}
+        controls={false}
+        progress={false}
+        theme={theme}
+      >
+        <Slide transition={['zoom']} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Spectacle Boilerplate
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            open the presentation/index.js file to get started
+          </Text>
+        </Slide>
+        <Slide transition={['fade']} bgColor="tertiary">
+          <Heading size={6} textColor="primary" caps>
+            Typography
+          </Heading>
+          <Heading size={1} textColor="secondary">
+            Heading 1
+          </Heading>
+          <Heading size={2} textColor="secondary">
+            Heading 2
+          </Heading>
+          <Heading size={3} textColor="secondary">
+            Heading 3
+          </Heading>
+          <Heading size={4} textColor="secondary">
+            Heading 4
+          </Heading>
+          <Heading size={5} textColor="secondary">
+            Heading 5
+          </Heading>
+          <Text size={6} textColor="secondary">
+            Standard text
+          </Text>
+        </Slide>
+        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
+          <Heading size={6} textColor="secondary" caps>
+            Standard List
+          </Heading>
+          <List>
+            <ListItem>Item 1</ListItem>
+            <ListItem>Item 2</ListItem>
+            <ListItem>Item 3</ListItem>
+            <ListItem>Item 4</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+          <BlockQuote>
+            <Quote>Example Quote</Quote>
+            <Cite>Author</Cite>
+          </BlockQuote>
+        </Slide>
+      </Deck>
+    );
+  }
+}
