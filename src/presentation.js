d4a6ed429eaa79cb6e3c957c7d36f5baf46dba65
Trygve <tnordberg@smm.org>
2018-02-09 17:44:18 -0600

    Testing different slide types

--- src/presentation.js
+++ src/presentation.js
@@ -1,5 +1,6 @@
 // Import React
 import React from 'react';
+import cueServer from './cueServer';
 
 // Import Spectacle Core tags
 import {
@@ -9,8 +10,10 @@ import {
   Heading,
   ListItem,
   List,
+  Appear,
   Quote,
   Image,
+  Notes,
   Slide,
   Text,
 } from 'spectacle';
@@ -26,72 +29,132 @@ const theme = createTheme(
     primary: '#F6C27C',
     secondary: '#24DDB4',
     tertiary: '#FAF97B',
-    quartenary: '#F49CB4',
+    quartenary: '#555666',
+    correct:'#33ddbc',
   },
   {
-    primary: 'Georgia',
+    primary: 'Verdana',
     secondary: 'Helvetica',
   }
 );
 
+const images = {
+
+  moths: require('./images/moths.gif'),
+  youtube: require('./images/youtube.png'),
+  tribble: require('./images/tribble.png'),
+  sequence1: require('./images/sequence1.png'),
+  sequence2: require('./images/sequence2.png'),
+  sequence3: require('./images/sequence3.png'),
+  evolution: require('./images/evolution.png'),
+
+};
+
 export default class Presentation extends React.Component {
+
+  onSlideActive(slideIndex) {
+
+    console.log('onSlideActive');
+    console.log(slideIndex);
+
+    // TODO - detect if slide contains
+    // sound or video and autoplay.
+
+    cueServer.loadCueByIndex(slideIndex);
+
+  }
+
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
-        transitionDuration={500}
+        transition={['fade']}
+        transitionDuration={300}
         controls={false}
         progress={false}
         theme={theme}
       >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+        <Slide bgColor='quartenary' onActive={this.onSlideActive}>
+
+          <Heading size={1} fit caps lineHeight={1} textColor='secondary'>
+            Guest lecture starting soon.
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Text margin='10px 0 0' textColor='tertiary' size={1} fit bold>
+            For your own safety, do NOT approach cages.
           </Text>
+
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
+
+        <Slide bgColor='secondary' notes='ENTER: Alex Makes It Amazing' onActive={this.onSlideActive}>
+
+          <Heading size={1} fit caps lineHeight={1} textColor='tertiary'>
+            ALEX MAKES IT AMAZING!
           </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
+
+          <Image src={images.youtube}></Image>
+
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+
+        <Slide bgColor='tertiary' notes='First quiz questions' onActive={this.onSlideActive}>
+
+          <Heading textColor='quartenary' fit={true}>If you had 100 VISTAs of your very own—you lucky dogs—what genetic variation might you observe among them?</Heading>
+          <br/>
+          <List ordered type='A' textColor='quartenary'>
+            <ListItem>VISTAs who have encountered humans more often will be friendlier than other VISTAs.</ListItem>
+            <br/>
+            <ListItem>Some VISTAs will be born friendly. Others, not.</ListItem>
+            <br/>
+            <ListItem>They are all VISTAs. They are all the same.</ListItem>
+            <br/>
           </List>
+
         </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+
+        <Slide bgColor='tertiary' notes='First quiz questions' onActive={this.onSlideActive}>
+
+          <Heading textColor='quartenary' fit={true}>If you had 100 VISTAs of your very own—you lucky dogs—what genetic variation might you observe among them?</Heading>
+          <br/>
+          <List ordered type='A' textColor='quartenary'>
+            <ListItem>VISTAs who have encountered humans more often will be friendlier than other VISTAs.</ListItem>
+            <br/>
+            <ListItem textColor='correct'>Some VISTAs will be born friendly. Others, not.</ListItem>
+            <br/>
+            <ListItem>They are all VISTAs. They are all the same.</ListItem>
+            <br/>
+          </List>
+
         </Slide>
+
+        <Slide>
+          <Notes>
+            <h4>Slide notes</h4>
+            <ol>
+              <li>First note</li>
+              <li>Second note</li>
+            </ol>
+
+          </Notes>
+          {/* Slide content */}
+          <p>My slide content(see notes)</p>
+        </Slide>
+
+        <Slide bgImage={images.tribble} notes='Slide notes here.' onActive={this.onSlideActive}></Slide>
+        <Slide bgImage={images.evolution} notes='Listen up, Alex Makes It Amazing fans: like the theory of gravity, the theory of evolution is sound science.'></Slide>
+        <Slide bgImage={images.sequence1} notes='Slide notes here.' onActive={this.onSlideActive}></Slide>
+        <Slide bgImage={images.sequence2} notes='Slide notes here.' onActive={this.onSlideActive}></Slide>
+        <Slide bgImage={images.sequence3} notes='Slide notes here.' onActive={this.onSlideActive}></Slide>
+
+        <Slide bgColor='quartenary' onActive={this.onSlideActive}>
+
+          <Heading size={1} fit caps lineHeight={1} textColor='primary'>
+            Isolation mode:
+          </Heading>
+
+          <Heading size={1} fit caps lineHeight={2} textColor='secondary'>
+            DISENGAGED
+          </Heading>
+
+        </Slide>
+
       </Deck>
     );
   }
